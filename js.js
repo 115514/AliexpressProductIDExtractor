7cbdd0e7496e79219e05683a4ba6fc2060e4c7ef
张续 <zhangxu@zhangxudeMacBook-Pro.local>
2023-09-20 10:14:19 +0800

    修改

--- js.js
+++ js.js
@@ -1,18 +1,21 @@
 // ==UserScript==
-// @name        Aliexpress Product ID Extractor
-// @namespace   Violentmonkey Scripts
-// @match       https://www.aliexpress.com/*
-// @grant       none
-// @version     1.1
-// @description Extract product IDs from Aliexpress and send them to a server
-// @author      GPT-4
+// @name         Aliexpress Product ID Extractor
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Extract product IDs from Aliexpress search page
+// @author       ChatGPT
+// @match        https://www.aliexpress.com/*
+// @grant        GM_xmlhttpRequest
 // ==/UserScript==
 
-(function () {
+(function() {
     'use strict';
 
+    // Variables to store product IDs
+    let productIds = JSON.parse(localStorage.getItem("productIds") || "[]");
+
     // Create a container to hold all buttons
-    var container = document.createElement('div');
+    const container = document.createElement('div');
     container.style.position = 'fixed';
     container.style.top = '10px';
     container.style.right = '10px';
@@ -23,44 +26,35 @@
     container.style.color = '#fff';
     document.body.appendChild(container);
 
-    // Create a button to send and save product IDs
-    var sendButton = document.createElement('input');
-    sendButton.type = 'button';
-    sendButton.value = 'Send and Save Product IDs';
-    sendButton.style.backgroundColor = '#333';
-    sendButton.style.margin = '10px';
-    sendButton.style.color = 'white';
-    container.appendChild(sendButton);
-    container.appendChild(document.createElement('br')); // New line
+    const sendAndAddButton = document.createElement('input');
+    sendAndAddButton.type = 'button';
+    sendAndAddButton.value = 'Send & Add Product IDs';
+    sendAndAddButton.style.backgroundColor = '#333';
+    sendAndAddButton.style.margin = '10px';
+    sendAndAddButton.style.color = 'white';
+    container.appendChild(sendAndAddButton);
 
-    // Create a button to clear saved product IDs
-    var clearButton = document.createElement('input');
-    clearButton.type = 'button';
-    clearButton.value = 'Clear Product IDs';
-    clearButton.style.backgroundColor = '#333';
-    clearButton.style.margin = '10px';
-    clearButton.style.color = 'white';
-    container.appendChild(clearButton);
-    container.appendChild(document.createElement('br')); // New line
-
-    // Create a button to export saved product IDs
-    var exportButton = document.createElement('input');
+    const exportButton = document.createElement('input');
     exportButton.type = 'button';
     exportButton.value = 'Export Product IDs';
     exportButton.style.backgroundColor = '#333';
     exportButton.style.margin = '10px';
     exportButton.style.color = 'white';
     container.appendChild(exportButton);
-    container.appendChild(document.createElement('br')); // New line
 
-    // Create a counter to show the number of saved product IDs
-    var counter = document.createElement('div');
-    counter.style.marginTop = '10px';
+    const clearButton = document.createElement('input');
+    clearButton.type = 'button';
+    clearButton.value = 'Clear Product IDs';
+    clearButton.style.backgroundColor = '#333';
+    clearButton.style.margin = '10px';
+    clearButton.style.color = 'white';
+    container.appendChild(clearButton);
+
+    const counter = document.createElement('div');
+    counter.textContent = `Count: ${productIds.length}`;
     container.appendChild(counter);
-    container.appendChild(document.createElement('br')); // New line
 
-    // Create a notification element
-    var notification = document.createElement('div');
+    const notification = document.createElement('div');
     notification.style.position = 'fixed';
     notification.style.top = '180px';
     notification.style.right = '10px';
@@ -72,75 +66,63 @@
     notification.style.display = 'none';
     document.body.appendChild(notification);
 
-    // Helper function to show a notification
-    function showNotification(message) {
-        notification.innerText = message;
+    const showNotification = (message) => {
+        notification.textContent = message;
         notification.style.display = 'block';
-        setTimeout(function () {
+        setTimeout(() => {
             notification.style.display = 'none';
-        }, 3000);
-    }
+        }, 2000);
+    };
 
-    // Initialize saved product IDs from localStorage
-    var productIds = JSON.parse(localStorage.getItem('productIds')) || [];
+    sendAndAddButton.addEventListener('click', () => {
+        const currentProductIds = Array.from(document.querySelectorAll('[data-product-id]'))
+            .map(el => parseInt(el.getAttribute('data-product-id')));
 
-    // Update the counter
-    counter.innerText = 'Saved Product IDs: ' + productIds.length;
+        if (currentProductIds.length === 0) {
+            showNotification("No product IDs found!");
+            return;
+        }
 
-    // Function to extract product IDs from the current page
-    function extractProductIds() {
-        var links = document.querySelectorAll('a');
-        links.forEach(function (link) {
-            var href = link.href;
-            var match = href.match(/(\d{10,})/);
-            if (match) {
-                var productId = match[1];
-                if (productIds.indexOf(productId) === -1) {
-                    productIds.push(productId);
+        GM_xmlhttpRequest({
+            method: "POST",
+            url: "http://10.1.1.100:7210/product/from-tamper-monkey",
+            data: JSON.stringify({
+                "ids": currentProductIds
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            },
+            onload: (response) => {
+                if (response.status === 200) {
+                    showNotification("Product IDs sent successfully!");
+                    productIds = productIds.concat(currentProductIds);
+                    localStorage.setItem("productIds", JSON.stringify(productIds));
+                    counter.textContent = `Count: ${productIds.length}`;
+                } else {
+                    showNotification("Failed to send product IDs!");
                 }
             }
         });
-        localStorage.setItem('productIds', JSON.stringify(productIds));
-        counter.innerText = 'Saved Product IDs: ' + productIds.length;
-    }
+    });
 
-    // Function to send product IDs to the server
-    function sendProductIds() {
-        extractProductIds();
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'http://10.1.1.100:7210/product/from-tamper-monkey', true);
-        xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == XMLHttpRequest.DONE) {
-                if (xhr.status == 200) {
-                    showNotification('Product IDs sent successfully!');
-                } else {
-                    showNotification('Error sending product IDs: ' + xhr.statusText);
-                }
-            }
-        };
-        xhr.send(JSON.stringify({ ids: productIds }));
-    }
+    exportButton.addEventListener('click', () => {
+        const blob = new Blob([productIds.join('\n')], {type: 'text/plain;charset=utf-8'});
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.style.display = 'none';
+        a.href = url;
+        a.download = 'productIds.txt';
 
-    // Function to clear saved product IDs
-    function clearProductIds() {
+        document.body.appendChild(a);
+        a.click();
+
+        window.URL.revokeObjectURL(url);
+    });
+
+    clearButton.addEventListener('click', () => {
         productIds = [];
-        localStorage.setItem('productIds', JSON.stringify(productIds));
-        counter.innerText = 'Saved Product IDs: ' + productIds.length;
-        showNotification('Saved Product IDs cleared!');
-    }
-
-    // Function to export saved product IDs
-    function exportProductIds() {
-        var blob = new Blob([productIds.join('\n')], { type: 'text/csv' });
-        var link = document.createElement('a');
-        link.href = window.URL.createObjectURL(blob);
-        link.download = 'product_ids.csv';
-        link.click();
-    }
-
-    // Add event listeners
-    sendButton.addEventListener('click', sendProductIds);
-    clearButton.addEventListener('click', clearProductIds);
-    exportButton.addEventListener('click', exportProductIds);
+        localStorage.setItem("productIds", "[]");
+        counter.textContent = `Count: ${productIds.length}`;
+        showNotification("Product IDs cleared!");
+    });
 })();
